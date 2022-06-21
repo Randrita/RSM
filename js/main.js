51971721c365275bbe8edd2e64cf68d388ec2a4b
Randrita Sarkar <60352282+Randrita@users.noreply.github.com>
2022-06-22 05:10:04 +0530

    website added

---
+++ js/main.js
@@ -0,0 +1,50 @@
+$(document).ready(function(){
+    /*---------video popup---------*/
+    const videoSrc=$("#player-1").attr("src");
+    $(".video-play-btn, .video-popup").on("click",function(){
+        if($(".video-popup").hasClass("open")){
+            $(".video-popup").removeClass("open");
+            $("#player-1").attr("src","");
+        }
+        else{
+            $(".video-popup").addClass("open");
+            if($("#player-1").attr("src")==''){
+                $("#player-1").attr("src",videoSrc);
+            }
+        }
+    });
+
+   /*------carousel--------*/
+   $('.features-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    autoplay:true,
+    responsiveClass:true,
+    responsive:{
+        0:{
+            items:1,
+            
+        },
+        600:{
+            items:2
+        },
+        1000:{
+            items:3,
+        }
+    }
+    });
+
+    /*-----testimonials------*/
+    $(".testimonial-content").owlCarousel({
+        loop: true,
+        items: 2,
+        margin: 50,
+        dots: true,
+        nav: false,
+        mouseDrag: true,
+        autoplay: false,
+        autoplayTimeout: 4000,
+        smartSpeed: 800
+    });  
+
+});
