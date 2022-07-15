1b7d39e755490f022e40dc8fffdfe33910a779f7
Randrita Sarkar <60352282+Randrita@users.noreply.github.com>
2022-07-16 02:03:24 +0530

    web updated

--- js/main.js
+++ js/main.js
@@ -37,14 +37,27 @@ $(document).ready(function(){
     /*-----testimonials------*/
     $(".testimonial-content").owlCarousel({
         loop: true,
-        items: 2,
+        
         margin: 50,
         dots: true,
         nav: false,
         mouseDrag: true,
         autoplay: false,
         autoplayTimeout: 4000,
-        smartSpeed: 800
+        smartSpeed: 800,
+        responsiveClass:true,
+        responsive:{
+            0:{
+                items:1,
+                
+            },
+            600:{
+                items:2
+            },
+            1000:{
+                items:3,
+            }
+        }
     });  
 
     /*-------------Counter Faculty-------*/
